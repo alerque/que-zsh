a2d3dc404b19ec4061a0e65ae426c373c4de5529
Caleb Maclennan <caleb@alerque.com>
2019-02-28 07:54:14 +0000

    Force GPG agent, documentation is wrong about it being inferred

--- bin/que-auth.zsh
+++ bin/que-auth.zsh
@@ -1,10 +1,11 @@
 #!/usr/bin/env zsh
 
 keychain \
+	--agents gpg,ssh \
 	--eval \
 	--quick \
 	--gpg2 \
 	--systemd \
 	--quiet \
 	caleb \
-	9F377DDB6D3153A48EB3EB1E63CC496475267693
+	63CC496475267693
